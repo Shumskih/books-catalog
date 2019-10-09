d8771239c7681bc4e65067c86d780de9ce6cfa81
Aleksander <alex.shumskih@gmail.com>
2019-10-09 11:50:03 +0300

    refactoring

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,38 +17,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->deleteAndCreateDir();
-        $this->createDir();
         factory(\App\Models\Author::class, 15)->create();
         factory(\App\Models\Book::class, 27)->create();
         factory(User::class, 1)->create();
         factory(Role::class, 1)->create();
         for ($i = 1; $i <= 27; $i++) {
             DB::table('authors_books')->insert([
-                'author_id' => random_int(1,15),
-                'book_id' => random_int(1,27)
+                'author_id' => random_int(1, 15),
+                'book_id'   => random_int(1, 27),
             ]);
         }
         DB::table('users_roles')->insert([
             'user_id' => 1,
-            'role_id' => 1
+            'role_id' => 1,
         ]);
     }
 
-    private function deleteAndCreateDir() {
+    private function deleteAndCreateDir()
+    {
         if (file_exists('./public/uploads/')) {
             $this->emptyDir();
             array_map('rmdir', glob('./public/uploads/'));
             mkdir('./public/uploads/', 0777, true);
-        }
-    }
-
-    private function createDir() {
-        if (!file_exists('./public/uploads/')) {
+        } else {
             mkdir('./public/uploads/', 0777, true);
         }
     }
 
-    private function emptyDir() {
+    private function emptyDir()
+    {
         foreach (glob('./public/uploads/*') as $file) {
             unlink($file);
         }

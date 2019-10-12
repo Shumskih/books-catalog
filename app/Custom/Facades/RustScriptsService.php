f03fa43612c0009cd4e2a48aea45a2852e51cdb0
Aleksander <alex.shumskih@gmail.com>
2019-10-12 16:16:27 +0300

    Implemented 'php artisan rustScripts:run' to compile rust scripts and create and delete uploads folder

--- app/Custom/Facades/RustScriptsService.php
+++ app/Custom/Facades/RustScriptsService.php
@@ -4,6 +4,17 @@ namespace App\Custom\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void compile()
+ * @method static bool isScriptsExists()
+ * @method static void createFolder()
+ * @method static void removeFolder()
+ * @method static void createScriptDir()
+ * @method static void removeScriptDir()
+ * @method static bool isUploadsDirExists()
+ * @method static string getUploadsDir()
+ *
+ */
 class RustScriptsService extends Facade
 {
     protected static function getFacadeAccessor()
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Custom\Services\RustScripts\RustScriptFactory;
 use App\User;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
@@ -17,30 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $rustScripts = RustScripts::factory();
-        if (!$rustScripts->isScriptsExists()) {
-            $rustScripts->compile();
-        }
-        if (!$rustScripts->isUploadsDirExists()) {
-            $rustScripts->createFolder();
-        } else {
-            $rustScripts->removeFolder();
-            $rustScripts->createFolder();
-        }
+//        $rustScripts = RustScripts::factory();
+//        if (!$rustScripts->isScriptsExists()) {
+//            $rustScripts->compile();
+//        }
+//        if (!$rustScripts->isUploadsDirExists()) {
+//            $rustScripts->createFolder();
+//        } else {
+//            $rustScripts->removeFolder();
+//            $rustScripts->createFolder();
+//        }
 
-        factory(\App\Models\Author::class, 15)->create();
-        factory(\App\Models\Book::class, 27)->create();
-        factory(User::class, 1)->create();
-        factory(Role::class, 1)->create();
-        for ($i = 1; $i <= 27; $i++) {
-            DB::table('authors_books')->insert([
-                'author_id' => random_int(1, 15),
-                'book_id'   => random_int(1, 27),
-            ]);
-        }
-        DB::table('users_roles')->insert([
-            'user_id' => 1,
-            'role_id' => 1,
-        ]);
+//        factory(\App\Models\Author::class, 15)->create();
+//        factory(\App\Models\Book::class, 27)->create();
+//        factory(User::class, 1)->create();
+//        factory(Role::class, 1)->create();
+//        for ($i = 1; $i <= 27; $i++) {
+//            DB::table('authors_books')->insert([
+//                'author_id' => random_int(1, 15),
+//                'book_id'   => random_int(1, 27),
+//            ]);
+//        }
+//        DB::table('users_roles')->insert([
+//            'user_id' => 1,
+//            'role_id' => 1,
+//        ]);
     }
 }

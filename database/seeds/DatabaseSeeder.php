227993cb1f15a1ff1865193ccbc3374903f0b359
Aleksander <alex.shumskih@gmail.com>
2019-10-12 14:40:40 +0300

    RustScripts service refactor to OOP, abstract class, inheritance, factory pattern

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,19 +27,20 @@ class DatabaseSeeder extends Seeder
             $rustScripts->removeFolder();
             $rustScripts->createFolder();
         }
-        //        factory(\App\Models\Author::class, 15)->create();
-        //        factory(\App\Models\Book::class, 27)->create();
-        //        factory(User::class, 1)->create();
-        //        factory(Role::class, 1)->create();
-        //        for ($i = 1; $i <= 27; $i++) {
-        //            DB::table('authors_books')->insert([
-        //                'author_id' => random_int(1, 15),
-        //                'book_id'   => random_int(1, 27),
-        //            ]);
-        //        }
-        //        DB::table('users_roles')->insert([
-        //            'user_id' => 1,
-        //            'role_id' => 1,
-        //        ]);
+
+        factory(\App\Models\Author::class, 15)->create();
+        factory(\App\Models\Book::class, 27)->create();
+        factory(User::class, 1)->create();
+        factory(Role::class, 1)->create();
+        for ($i = 1; $i <= 27; $i++) {
+            DB::table('authors_books')->insert([
+                'author_id' => random_int(1, 15),
+                'book_id'   => random_int(1, 27),
+            ]);
+        }
+        DB::table('users_roles')->insert([
+            'user_id' => 1,
+            'role_id' => 1,
+        ]);
     }
 }
